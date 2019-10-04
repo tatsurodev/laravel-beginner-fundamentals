52ca8bae7c25b6c3d90d5384cc2b6f60ec72e69b
Tatsuro <contacts@tatsuro.dev>
2019-10-05 03:44:59 +0900

    migrationの設定 12

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            // string型は、マイグレーションの際、mysqlのvarchar(255)に変換される
+            // indexのデータ長は767までなので、utf8mb4のバイト数の4 掛ける 文字数の255 = 1020だと上限を超えてしまうので文字数を減らす必要がある
+            $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
