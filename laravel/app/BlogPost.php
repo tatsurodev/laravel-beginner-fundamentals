6533520050e429859cdd5d28586d6eaf75e6d089
Tatsuro <contacts@tatsuro.dev>
2019-10-05 14:27:10 +0900

    モデルの作成、migrationでテーブル名をrename 14

---
+++ laravel/app/BlogPost.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlogPost extends Model
+{
+    // 下記で関連付けるテーブル名を指定、BlogPostモデルはデフォルトだとlaravelが自動的にblog_postsテーブルを参照する
+    // protected $table = 'blogposts';
+}
