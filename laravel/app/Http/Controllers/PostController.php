192c9ed98e9bbefc47c6e112ff58e3a1ea7b9a46
Tatsuro <contacts@tatsuro.dev>
2019-10-07 01:19:10 +0900

    PostController@update作成 27

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -68,6 +68,14 @@ class PostController extends Controller
         return view('posts.edit', ['post' => $post]);
     }
 
-    public function update()
-    { }
+    public function update(StorePost $request, $id)
+    {
+        $post = BlogPost::findOrFail($id);
+        $validatedData = $request->validated();
+        // 既にあるinstanceに対するmass assignmentはfill methodを使用
+        $post->fill($validatedData);
+        $post->save();
+        $request->session()->flash('status', 'Blog post was updated!');
+        return redirect()->route('posts.show', ['post' => $post->id]);
+    }
 }
