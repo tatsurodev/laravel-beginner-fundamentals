a99e8653dbf8702f7ddf8ee73e1ecd4a44b33dfc
Tatsuro <contacts@tatsuro.dev>
2019-10-07 15:40:07 +0900

    laravel mix install 31
    laravel6からvueがdefaultでないので最初にinstallする
    composer require laravel/ui
    php artisan ui vue
    npm install
    npm run dev

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>Document</title>
 </head>
 
@@ -21,6 +22,7 @@
     <p style="color: green">{{ session()->get('status') }}</p>
     @endif
     @yield('content')
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
