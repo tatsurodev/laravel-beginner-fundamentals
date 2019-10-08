9c47594775b2b2d3784805bf891019bef04e3ee2
Tatsuro <contacts@tatsuro.dev>
2019-10-08 23:49:16 +0900

    afterCreating methodでauthor作成後にprofile relationをセットする 54

---
+++ laravel/database/factories/AuthorFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Author;
+use Faker\Generator as Faker;
+
+$factory->define(Author::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
+
+// afterCreating(model, callback)
+// afterCreatingであるmodel instanceがdbに保存された際にcallbackとして処理を行うことができる
+$facotyr->afterCreating(App\Author::class, function ($author, $faker) {
+    // authorが作成された後、profileをrelationを指定して保存する
+    $author->profile()->save(factory(App\Profile::class)->make());
+});
