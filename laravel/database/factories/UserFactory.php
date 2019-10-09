817b46c2b983b483ee1fd2d89e88896c7cd61398
Tatsuro <contacts@tatsuro.dev>
2019-10-10 00:52:33 +0900

    login用ユーザーをUserFactoryにadmin stateとして作成、seedで利用 70

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -23,5 +24,13 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+// loginに使用するadmin用state作成
+$factory->state(App\User::class, 'admin', function () {
+    return [
+        'name' => 'John Doe',
+        'email' => 'contacts@tatsuro.dev',
     ];
 });
--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use  Illuminate\Support\Facades\DB;
+// use  Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +13,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        DB::table('users')->insert([
-            'name' => 'John Doe',
-            'email' => 'contacts@tatsuro.dev',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10)
-        ]);
+        // UserFactoryにadmin stateとして作成
+        // DB::table('users')->insert([
+        //     'name' => 'John Doe',
+        //     'email' => 'contacts@tatsuro.dev',
+        //     'email_verified_at' => now(),
+        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //     'remember_token' => Str::random(10)
+        // ]);
+
+        // login用
+        factory(App\User::class)->states('admin')->create();
+
+        // UserFactory
+        factory(App\User::class, 20)->create();
     }
 }
