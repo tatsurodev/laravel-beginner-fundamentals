cf98dd47ab6a1ee0d30eea1ffd4177cefc7e7d7c
Tatsuro <contacts@tatsuro.dev>
2019-10-09 18:59:03 +0900

    blog_posts tableに誰が投稿したかわかるようにforeign keyのuser_idを追加する 67

---
+++ laravel/database/migrations/2019_10_09_091120_add_user_to_blog_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserToBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blog_posts', function (Blueprint $table) {
+            // 既存のblog_posts tableにuser_id fieldを追加するとデフォルト値がないのでエラーとなるのでnullableを指定する、art migrate:refreshの場合はdataをゼロから作るので不要
+            // $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blog_posts', function (Blueprint $table) {
+            // dropForeign(tablename_filedname_foreign), dropForeign([fieldname]): 引数を配列出ない方法で指定するとテーブル名とカラム名をつなげ"_foreign"を最後につけた引数を指定する必要があるので配列で指定する方が楽
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+    }
+}
