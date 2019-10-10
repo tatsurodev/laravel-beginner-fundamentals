e4bd70b526dd08e85c2ec8a3ee20de5bf5a75676
Tatsuro <contacts@tatsuro.dev>
2019-10-10 17:48:54 +0900

    seederでrelationを含んだusers, posts, commentsのdummy dataを作成 71

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -22,10 +22,30 @@ class DatabaseSeeder extends Seeder
         //     'remember_token' => Str::random(10)
         // ]);
 
-        // login用
-        factory(App\User::class)->states('admin')->create();
+        // login用ユーザー
+        $admin = factory(App\User::class)->states('admin')->create();
 
-        // UserFactory
-        factory(App\User::class, 20)->create();
+        // その他ダミーユーザー
+        $else = factory(App\User::class, 20)->create();
+
+        // dd(get_class($admin)); //"App\User"
+        // dd(get_class($else); // "Illuminate\Database\Eloquent\Collection"
+
+        // 作成した全ユーザーのcollection作成
+        $users = $else->concat([$admin]);
+        // dd($users->count());
+
+        // postsをmakeし、それぞれのpostにuser_idを割り当ててsave。postsをsaveだといきなりdataを作成することになり、user_idがないのでエラーとなってしまうので、メモリー上に一時的に作成するmakeを使う
+        // eachの中のclosureは外の変数にアクセスできないのでuse ($variable)でアクセスできるようにする
+        $posts = factory(App\BlogPost::class, 50)->make()->each(function ($post) use ($users) {
+            $post->user_id = $users->random()->id;
+            $post->save();
+        });
+
+        // commentsをmakeし、それぞれのcommentにpost_idを割り当ててsave
+        $comments = factory(App\Comment::class, 150)->make()->each(function ($comment) use ($posts) {
+            $comment->blog_post_id = $posts->random()->id;
+            $comment->save();
+        });
     }
 }
