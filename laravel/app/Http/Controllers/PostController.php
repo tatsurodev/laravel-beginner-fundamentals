ae33933bba9c9a33ec1d4c545de97b9d3d8d0ade
Tatsuro <contacts@tatsuro.dev>
2019-10-15 05:49:18 +0900

    mostActiveLastMonth scope作成 96

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -59,6 +59,7 @@ class PostController extends Controller
             'posts' => BlogPost::latest()->withCount('comments')->get(),
             'mostCommented' => BlogPost::mostCommented()->take(5)->get(),
             'mostActive' => User::withMostBlogPosts()->take(5)->get(),
+            'mostActiveLastMonth' => User::withMostBlogPostsLastMonth()->take(5)->get(),
         ]);
     }
 
