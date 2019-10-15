5fb1a3422e061d01473341592ec558ba6176f523
Tatsuro <contacts@tatsuro.dev>
2019-10-15 18:02:04 +0900

    badge componentにaliasを作成して使用 99

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // componentにaliasを作成する、Blade::component(component path, alias name)
+        Blade::component('components.badge', 'badge');
     }
 }
--- laravel/resources/views/posts/show.blade.php
+++ laravel/resources/views/posts/show.blade.php
@@ -7,14 +7,16 @@
     <p>Added {{ $post->created_at->diffForHumans() }}</p>
 
     {{-- 現在時刻をnew Carbon\Carbon()で取得し、diffInMinutesメソッドの引数との差分が5以下ならNew!と表示 --}}
-    @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 75)
+    @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 995)
         {{-- slotへdataの配列を渡す --}}
-        @component('badge', ['type' => 'primary'])
+        {{-- @badge(['type' => 'primary']) --}}
+        {{-- type slotを渡していないのでdefault値が使用される --}}
+        @badge
             Brand new Post!
-        @endcomponent
+        @endbadge
         {{-- slot directiveを使ってslot変数を渡す --}}
         <!--
-        @component('badge')
+        @component('components.badge')
             Brand new Post!
             @slot('type')
                 primary
