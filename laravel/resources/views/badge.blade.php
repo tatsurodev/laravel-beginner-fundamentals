b522353dcaa868dba4d59c5ea62983f7531ac6ed
Tatsuro <contacts@tatsuro.dev>
2019-10-15 17:13:04 +0900

    slotの作成とslotへのdataの渡し方 98

---
+++ laravel/resources/views/badge.blade.php
@@ -0,0 +1,3 @@
+<div class="badge badge-{{ $type ?? 'success'}}">
+    {{ $slot }}
+</div>
--- laravel/resources/views/posts/show.blade.php
+++ laravel/resources/views/posts/show.blade.php
@@ -7,8 +7,20 @@
     <p>Added {{ $post->created_at->diffForHumans() }}</p>
 
     {{-- 現在時刻をnew Carbon\Carbon()で取得し、diffInMinutesメソッドの引数との差分が5以下ならNew!と表示 --}}
-    @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 5)
-        <strong>New!</strong>
+    @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 75)
+        {{-- slotへdataの配列を渡す --}}
+        @component('badge', ['type' => 'primary'])
+            Brand new Post!
+        @endcomponent
+        {{-- slot directiveを使ってslot変数を渡す --}}
+        <!--
+        @component('badge')
+            Brand new Post!
+            @slot('type')
+                primary
+            @endslot
+        @endcomponent
+        -->
     @endif
 
     <h4>Comments</h4>
