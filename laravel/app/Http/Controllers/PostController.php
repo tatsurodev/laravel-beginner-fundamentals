35e98d99e55b06852bf372ebf0de60cea2053417
Tatsuro <contacts@tatsuro.dev>
2019-10-17 00:17:42 +0900

    currently readのcounterをcacheで設置 110

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -96,7 +96,50 @@ class PostController extends Controller
             )->findOrFail($id);
         });
 
-        $counter = 0;
+        // session idを取得
+        $sessionId = session()->getId();
+        // 閲覧中user数を保存するキー
+        $counterKey = "blog-post-{$id}-counter";
+        // 閲覧中のusersを配列に保存するキー
+        $usersKey = "blog-post-{$id}-users";
+        // usersをキャッシュから復元、キーがなければ初アクセスなので空配列セット
+        $users = Cache::get($usersKey, []);
+        // アクセスしてきたuserを保存する配列で、一定時間(この場合1分)を超えていたら削除する
+        $usersUpdate = [];
+        // counterの増減
+        $diffrence = 0;
+        // 現時刻
+        $now = now();
+
+        // usersを一定時間超えていたらdiffrenceを-1、そうでなければcacheに保存する用の変数にsession idと現時間格納
+        foreach ($users as $session => $lastVisit) {
+            if ($now->diffInMinutes($lastVisit) >= 1) {
+                $diffrence--;
+            } else {
+                $usersUpdate[$session] = $lastVisit;
+            }
+        }
+
+        // 初アクセス or アクセス済だが一定時間を超えていた場合、diffrenceを+1
+        if (!array_key_exists($sessionId, $users) || $now->diffInMinutes($users[$sessionId]) >= 1) {
+            $diffrence++;
+        }
+
+        // 現ユーザーのアクセス時間を更新
+        $usersUpdate[$sessionId] = $now;
+
+        // usersをcacheに保存
+        Cache::forever($usersKey, $usersUpdate);
+
+        // counterをcacheに保存
+        if (!Cache::has($counterKey)) {
+            Cache::forever($counterKey, 1);
+        } else {
+            Cache::increment($counterKey, $diffrence);
+        }
+
+        // 現counterの値を取得
+        $counter = Cache::get($counterKey);
 
         return view('posts.show', ['post' => $blogPost, 'counter' => $counter,]);
     }
