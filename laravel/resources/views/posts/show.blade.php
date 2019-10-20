453d854466804eed674404b5d7fb142789ad987e
Tatsuro <contacts@tatsuro.dev>
2019-10-21 06:52:41 +0900

    uploadした画像の表示 135

--- laravel/resources/views/posts/show.blade.php
+++ laravel/resources/views/posts/show.blade.php
@@ -23,6 +23,15 @@
             -->
         </h1>
         <p>{{ $post->content }}</p>
+
+        {{--
+        <img src="http://laravel.test/storage/{{ $post->image->path }}">
+        <img src="{{ asset($post->image->path) }}"> <!-- js, css等でよく使用するapp_root/publicへのリンク -->
+        <!-- 上記の２つはdisk('public')等保存場所を変更した場合影響を受けるが、下２つの方法は.envのFILESYSTEM_DRIVERの変更、もしくはStorage::disk('変更場所')->url()で変更できるため変更に強い -->
+        <img src="{{ Storage::url($post->image->path) }}">
+        --}}
+        <img src="{{ $post->image->url() }}">
+
         {{-- diffForHumansで人にわかりやすい形式で表示 --}}
         @updated(['date' => $post->created_at, 'name' => $post->user->name])
         @endupdated
