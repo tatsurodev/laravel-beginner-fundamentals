95e031465db5b5ae1e6fb401a04018e05ccfa867
Tatsuro <contacts@tatsuro.dev>
2019-10-27 04:00:29 +0900

    Tag modelを多対多のpolymorphicに対応されるmigration file作成 154
    art make:migration RenameBlogPostTagTableToTaggables --table=blog_post_tag

--- laravel/app/Tag.php
+++ laravel/app/Tag.php
@@ -9,6 +9,7 @@ class Tag extends Model
     public function blogPosts()
     {
         // withTimestampsでtimestamps追加、asで中間テーブル名をpivotから変更
-        return $this->belongsToMany('App\BlogPost')->withTimestamps()->as('tagged');
+        // return $this->belongsToMany('App\BlogPost')->withTimestamps()->as('tagged');
+        return $this->morphedByMany('App\BlogPost', 'taggable')->withTimestamps()->as('tagged');
     }
 }
---
+++ laravel/database/migrations/2019_10_26_174440_rename_blog_post_tag_table_to_taggables.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameBlogPostTagTableToTaggables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // table名を変更して外部キーを削除する時、最初に外部キーを削除してからtable名の変更をする。最初にtable名を変更してしまうと、dropForeign methodで変更前のtable名を使ってfullの外部キー名をlaravelが作ってしまうため
+        Schema::table('blog_post_tag', function (Blueprint $table) {
+            $table->dropForeign(['blog_post_id']);
+            $table->dropColumn('blog_post_id');
+        });
+        // table名をtaggablesにするのは、polymorphicにするTag modelで使用するmorphedByMany methodがdefaultで指定するprefix名を指定すると自動的にそれを複数形にしたtable名とリンクしてくれるため
+        Schema::rename('blog_post_tag', 'taggables');
+        Schema::table('taggables', function (Blueprint $table) {
+            $table->morphs('taggable');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('taggables', function (Blueprint $table) {
+            $table->dropMorphs('taggables');
+        });
+        Schema::rename('taggables', 'blog_post_tag');
+        Schema::disableForeignKeyConstraints();
+        Schema::table('blog_post_tag', function (Blueprint $table) {
+            $table->unsignedBigInteger('blog_post_id')->index();
+            $table->foreign('blog_post_id')->references('id')->on('blog_posts')->onDelete('cascade');
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+}
