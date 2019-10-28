3d5732cef81db73fb43375196bb383dd6af2a15e
Tatsuro <contacts@tatsuro.dev>
2019-10-28 11:25:16 +0900

    emailへのfile attachment 161

--- laravel/app/Mail/CommentPosted.php
+++ laravel/app/Mail/CommentPosted.php
@@ -6,6 +6,7 @@ use App\Comment;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 class CommentPosted extends Mailable
@@ -35,6 +36,22 @@ class CommentPosted extends Mailable
     {
         $subject = "Commented was posted on your {$this->comment->commentable->title} blog post";
         // view(ビュー名, 渡す配列);
-        return $this->subject($subject)->view('emails.posts.commented');
+        return $this
+            // file attachment
+            // first method
+            // ->attach(
+            //     storage_path('app/public') . '/' . $this->comment->commentable->image->path,
+            //     [
+            //         'as' => 'profile_picture.jpeg',
+            //         'mime' => 'image/jpeg'
+            //     ]
+            // )
+            // second method
+            // ->attachFromStorage($this->comment->commentable->image->path, 'profile_picture.jpeg')
+            // third method
+            // ->attachFromStorageDisk('public', $this->commentable->iamge->path)
+            // ->attachData(Storage::get($this->comment->commentable->image->path), 'profile_picture_from_data.jpeg', ['mime' => 'image/jpeg'])
+            ->subject($subject)
+            ->view('emails.posts.commented');
     }
 }
