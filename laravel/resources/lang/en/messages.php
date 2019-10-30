900e12e19e302b1d69668139f0ad389fb6e5c424
Tatsuro <contacts@tatsuro.dev>
2019-10-30 18:48:47 +0900

    singular, pluralによってtranslationを指定、また変数を渡す 183

---
+++ laravel/resources/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to Laravel!',
+    // :valueで変数を受け取れる
+    'example_with_value' => 'Hello :name',
+    // ちょうどの値 {}, 範囲 [from,to] *で無限
+    'plural' => '{0} No comments yet :a|{1} :count comments :a|[1,*] :count comments :a',
+];
--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,6 +1,17 @@
 @extends('layout')
 
 @section('content')
-<h1>Welcome to Laravel!</h1>
+{{-- __() helper method or @lang directive --}}
+<h1>{{ __('messages.welcome') }}</h1>
+<h1>@lang('messages.welcome')</h1>
+
+{{-- 第二引数の配列で変数を渡す --}}
+<p>{{ __('messages.example_with_value', ['name' => 'John']) }}</p>
+
+{{-- trans_choice methodで数に応じて単語を変化させる --}}
+<p>{{ trans_choice('messages.plural', 0, ['a' => 1]) }}</p>
+<p>{{ trans_choice('messages.plural', 1, ['a' => 1]) }}</p>
+<p>{{ trans_choice('messages.plural', 2, ['a' => 1]) }}</p>
+
 <p>This is the ontent of the main page!</p>
 @endsection
