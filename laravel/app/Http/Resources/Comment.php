e2ffa6743571ca3df10bf2feddc7cee4120ebf3a
Tatsuro <contacts@tatsuro.dev>
2019-11-11 20:02:59 +0900

    CommentResourceのrelationで使用するCommentUserResourceを作成 200
    art make:resource CommentUser

--- laravel/app/Http/Resources/Comment.php
+++ laravel/app/Http/Resources/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\CommentUser as CommentUserResource;
 
 class Comment extends JsonResource
 {
@@ -20,6 +21,8 @@ class Comment extends JsonResource
             // string型にしないとcarbonが返ってくる
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
+            // commen userのrelation用にresourceを作成、
+            'user' => new CommentUserResource($this->user),
         ];
     }
 }
