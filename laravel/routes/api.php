c3f94dd935ce16ac551435526013ff61fa5d0e3d
Tatsuro <contacts@tatsuro.dev>
2019-11-12 19:04:50 +0900

    api routeとcontroller作成 204
    art make:controller Api/V1/PostCommentController --api

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -17,3 +17,22 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// prefixの前にRouteServiceProviderのmapApiRoutes methodで指定されているprefixが使用される
+// name methodでこのグループのname prefixを指定
+// namespace methodでcontrollerのnamespaceを指定、RouteServiceProviderの$namespaceからの階層を指定
+Route::prefix('v1')->name('api.v1.')->namespace('Api\V1')->group(function () {
+    Route::get('/status', function () {
+        return response()->json(['status' => 'OK']);
+    })->name('status');
+    // apiResource methodでwebと同じようなresourcefulなrouteを指定できる、webとの違いはcreate, editがapiには不要なのでない
+    // route nameはこのグループのapi.v1.とposts.commentsを足したapi.v1.posts.commentsとなる
+    // controllerの場所は Api/V1/PostCommentController
+    Route::apiResource('posts.comments', 'PostCommentController');
+});
+
+Route::prefix('v2')->name('api.v2.')->group(function () {
+    Route::get('/status', function () {
+        return response()->json(['status' => true]);
+    })->name('status');
+});
